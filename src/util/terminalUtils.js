149d6d230820312ab14dc1dbc9542cc02900c5fc
DltFred <fredy.delatorre.frd@gmail.com>
2024-02-22 12:21:25 -0500

    Updates

--- src/util/terminalUtils.js
+++
@@ -1,7 +0,0 @@
-export const typeText = (textToType, setTextCallback) => {
-  for (let i = 0; i < textToType.length; i++) {
-    setTimeout(() => {
-      setTextCallback((prevText) => prevText + textToType[i]);
-    }, 100 * i);
-  }
-};
